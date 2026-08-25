d57aa512f101dd11de0acd6a9e64d8c5e10f5d8f
Prajwal Achwale <prajwalachwale29@gmail.com>
2026-08-25 23:52:09 +0530

    Add: is-subsequence - submission-0

---
+++ Data Structures & Algorithms/is-subsequence/submission-0.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        return rec(s, t, 0, 0);
+    }
+
+private:
+    bool rec(string& s, string& t, int i, int j) {
+        if (i == s.size()) return true;
+        if (j == t.size()) return false;
+        if (s[i] == t[j]) {
+            return rec(s, t, i + 1, j + 1);
+        }
+        return rec(s, t, i, j + 1);
+    }
+};
